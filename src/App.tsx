8bd558581842d0ae267974b4e598b3773d434d00
Mika Doorenbosch <mika.doorenbosch@gmail.com>
2023-05-24 11:03:26 +0200

    header update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Content from './components/Content'
 
 function App() {
   return (
     <>
       <Header/>
-      <div className="main">
-        Hello
-      </div>
+      <Content/>
       <Footer/>
     </>
   )
