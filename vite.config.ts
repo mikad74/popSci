c1ff04c64da01eb1dc0ca405eb29f7404b694b84
Mika Doorenbosch <mika.doorenbosch@gmail.com>
2023-05-25 16:16:13 +0200

    final skeleton

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from "vite-plugin-svgr"
+import webfontDownload from "vite-plugin-webfont-dl"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [svgr(), react(), webfontDownload()],
+  base: '/popSci/'
 })
