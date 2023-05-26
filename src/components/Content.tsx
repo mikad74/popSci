b330ad36a177e8bc577b57fc0bcfde8331dae924
mikad74 <mika.doorenbosch@gmail.com>
2023-05-26 13:17:26 +0200

    deploy

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -34,40 +34,78 @@ function Content() {
           While investigating this topic, we found roots of queerphobia in
           colonialism. Contrary to outdated beliefs of a civilising West,
           colonialism has been proven to be damaging even in the case of queer
-          freedoms, as mentioned by scholar Peter Jackson. He observes that
-          Thailand and Japan are the only East Asian countries where LGBTQ+
-          cultures developed in modern times and he relates it to the fact that
-          they did not suffer direct colonisation. The opposite is the case of
-          Mongolia, which underwent Western colonisation in the form of Soviet
-          imperialism, preventing these communities from sprouting.
+          freedoms, as mentioned by scholar
+          <a
+            href="https://www.cairn.info/revue-autrepart-2009-1-page-15.htm"
+            target="_blank"
+          >
+            {" "}
+            Peter Jackson
+          </a>
+          . He observes that Thailand and Japan are the only East Asian
+          countries where LGBTQ+ cultures developed in modern times and he
+          relates it to the fact that they did not suffer direct colonisation.
+          The opposite is the case of Mongolia, which underwent Western
+          colonisation in the form of Soviet imperialism, preventing these
+          communities from sprouting.
         </p>
         <p>
           Another example of queerphobia rooted in colonialism is found in Hong
           Kong, where homosexuality was criminalised by the British rulers. The
           stigma persists to this day to the point that queerness is portrayed
-          as being contrary to ‘traditional Chinese values’.
+          as being contrary to
+          <a
+            href="https://onlinelibrary.wiley.com/doi/abs/10.1111/weng.12005"
+            target="_blank"
+          >
+            {" "}
+            ‘traditional Chinese values’
+          </a>
+          .
         </p>
         <h5>English as a Safe Space</h5>
         <p>
           Queerphobic attitudes lead to a necessity for safe spaces. It’s been
-          observed in several studies that queer people tend to use English in
-          order to build what we might call a “linguistic safe space”.
+          observed in
+          <a
+            href="https://www.press.uillinois.edu/books/?id=p071423"
+            target="_blank"
+          >
+            {" "}
+            several studies{" "}
+          </a>
+          that queer people tend to use English in order to build what we might
+          call a “linguistic safe space”.
         </p>
         <p>
           The appropriation of English by queer communities involves countries
-          like Israel where the local language(s) don’t allow escaping the
-          gender binary due to their grammar. In this case, English can be
-          perceived as a safe space due the fact that it allows for gender
-          neutrality.
+          like
+          <a
+            href="https://books.google.nl/books?hl=en&lr=&id=lbMUxRiQO6sC&oi=fnd&pg=PA105&dq=moriel+israel+disco&ots=oj6Gqb6Hun&sig=zBl_76aEXmDoJoG7Jq-u8YlnZ3M&redir_esc=y#v=onepage&q=moriel%20israel%20disco&f=false"
+            target="_blank"
+          >
+            {" "}
+            Israel{" "}
+          </a>
+          where the local language(s) don’t allow escaping the gender binary due
+          to their grammar. In this case, English can be perceived as a safe
+          space due the fact that it allows for gender neutrality.
         </p>
         <p>
           On the other hand, Mongolia provides an interesting scenario, as the
           importance of the use of English on Facebook is shown by associate
           professor Dovchin to be an important component of the contemporary gay
-          Mongolian identity. A participant in the study described the feeling
-          of having found a safe space as “using English is more natural and
-          easier for me than using Mongolian because it gives me the freedom I
-          have always been searching for”.
+          Mongolian identity. A participant in
+          <a
+            href="https://onlinelibrary.wiley.com/doi/abs/10.1111/weng.12005"
+            target="_blank"
+          >
+            {" "}
+            the study{" "}
+          </a>
+          described the feeling of having found a safe space as “using English
+          is more natural and easier for me than using Mongolian because it
+          gives me the freedom I have always been searching for”.
         </p>
         <p>
           In addition, Dovchin pinpoints the form of English and
@@ -80,41 +118,75 @@ function Content() {
         <h5>Transnational English</h5>
         <p>
           Even outside of the virtual world, English is a tool for transnational
-          connections. Among queer Israelis, English is thought of as a way to
-          engage with like-minded people and take part in a global conversation.
-          Similarly, during the late 70s and 80s in Hong Kong, only a small
-          proportion of the population spoke English fluently, regarding it as
-          the language of foreigners. Thus, the reason to learn English for many
-          LGBTQ+ people at the time was mainly to provide a gateway to a more
-          liberal Western culture and finding a foreign partner in order to
-          escape the restrictions of their society. In other words, it allows
-          for a sense of inclusion and cosmopolitanism by enabling people to
-          express their identities in a way that transcends regional linguistic
-          and cultural boundaries.
+          connections. Among
+          <a
+            href="https://books.google.nl/books?hl=en&lr=&id=lbMUxRiQO6sC&oi=fnd&pg=PA105&dq=moriel+israel+disco&ots=oj6Gqb6Hun&sig=zBl_76aEXmDoJoG7Jq-u8YlnZ3M&redir_esc=y#v=onepage&q=moriel%20israel%20disco&f=false"
+            target="_blank"
+          >
+            {" "}
+            queer Israelis
+          </a>
+          , English is thought of as a way to engage with like-minded people and
+          take part in a global conversation. Similarly, during the late 70s and
+          80s in Hong Kong, only a small proportion of the population spoke
+          English fluently, regarding it as the
+          <a
+            href="https://onlinelibrary.wiley.com/doi/abs/10.1111/weng.12005"
+            target="_blank"
+          >
+            {" "}
+            language of foreigners
+          </a>
+          . Thus, the reason to learn English for many LGBTQ+ people at the time
+          was mainly to provide a gateway to a more liberal Western culture and
+          finding a foreign partner in order to escape the restrictions of their
+          society. In other words, it allows for a sense of inclusion and
+          cosmopolitanism by enabling people to express their identities in a
+          way that transcends regional linguistic and cultural boundaries.
         </p>
         <h5>The English Queer Creole</h5>
         <p>
           When LGBTQ+ people use English, they show creativity by either
           integrating it into their language or assigning novel meanings to
-          existing words. An example of the former is the way gay Mongolian men
-          use a word like “emotion”, ‘cut’ it into “emo-” then add the suffix
-          “-rood” (to become), forming emorood, which means “to become extremely
-          emotional”.
+          existing words. An example of the former is
+          <a
+            href="https://onlinelibrary.wiley.com/doi/10.1111/weng.12445"
+            target="_blank"
+          >
+            {" "}
+            the way gay Mongolian men use a word like “emotion”
+          </a>
+          , ‘cut’ it into “emo-” then add the suffix “-rood” (to become),
+          forming emorood, which means “to become extremely emotional”.
         </p>
         <p>
-          In the Thai gay community, queer concepts like “top” and “bottom” are
-          expressed with English words “(gay) king” and “(gay) queen”, in a way
-          that is not understandable by speakers of English. Likewise, the
-          lesbian community uses the words tom and dee, respectively from
-          “tomboy” and “lady” to express the pair “butch/femme”.
+          In the
+          <a
+            href="https://www.cairn.info/revue-autrepart-2009-1-page-15.htm"
+            target="_blank"
+          >
+            {" "}
+            Thai gay community
+          </a>
+          , queer concepts like “top” and “bottom” are expressed with English
+          words “(gay) king” and “(gay) queen”, in a way that is not
+          understandable by speakers of English. Likewise, the lesbian community
+          uses the words tom and dee, respectively from “tomboy” and “lady” to
+          express the pair “butch/femme”.
         </p>
         <p>
           As another example, within Israel's LGBTQ+ culture, there is a process
           of creolisation involving the blending of gay English, vernacular
           Hebrew, and Arabic, resulting in the creation of a unique local lingo
-          that often retains the Hebrew grammar. For instance, in lesbian
-          circles one is either “butch-eet” or “femme-eet” originating from the
-          Hebrew feminine ending.
+          that often retains the Hebrew grammar. For instance,
+          <a
+            href="https://books.google.nl/books?hl=en&lr=&id=lbMUxRiQO6sC&oi=fnd&pg=PA105&dq=moriel+israel+disco&ots=oj6Gqb6Hun&sig=zBl_76aEXmDoJoG7Jq-u8YlnZ3M&redir_esc=y#v=onepage&q=moriel%20israel%20disco&f=false"
+            target="_blank"
+          >
+            {" "}
+            in lesbian circles one is either “butch-eet” or “femme-eet”
+          </a>
+          originating from the Hebrew feminine ending.
         </p>
         <h5>Conclusion</h5>
         <p>Everybody is gay.</p>
